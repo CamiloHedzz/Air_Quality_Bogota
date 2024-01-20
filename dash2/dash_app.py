aea5046ed85efa9721e3745eedd2dac850820fd8
camilohedzz <camilohedzz@gmail.com>
2024-01-19 23:22:30 -0500

    callbacks

--- dash2/dash_app.py
+++ dash2/dash_app.py
@@ -1,10 +1,12 @@
 import json
 import pandas as pd
-import plotly.express as px
-import plotly.graph_objects as go
+import plotly.graph_objs as go
 from urllib.request import urlopen
 from django_plotly_dash import DjangoDash
-from dash import Dash, html, dcc, callback, Output, Input
+from dash import Dash, html, dcc, Output, Input
+
+
+app = DjangoDash('SimpleExample')
 
 
 with urlopen('https://gist.githubusercontent.com/john-guerra/ee93225ca2c671b3550d62614f4978f3/raw/b1d556c39f3d7b6e495bf26b7fda815765ac110a/bogota_cadastral.json') as response:
@@ -13,43 +15,56 @@ with urlopen('https://gist.githubusercontent.com/john-guerra/ee93225ca2c671b3550
 df = pd.read_csv("https://raw.githubusercontent.com/CamiloHedzz/Procesamiento-de-imagenes/main/bogota_cadastral2.csv",
                    dtype={"DISPLAY_NAME": str})
 
-app = DjangoDash('SimpleExample')
+fig = go.Figure(go.Choroplethmapbox(
+    geojson=counties,
+    locations=df.code,
+    z=df.sampl,
+    featureidkey='properties.DISPLAY_NAME',
+    colorscale="Viridis",
+    zmin=df.sampl.min(),
+    zmax=df.sampl.max(),
+    marker_opacity=0.5,
+    #marker_line_width=3,
+    #marker_line_color='aqua'
+))
 
+fig.update_layout(
+    mapbox_zoom=10,
+    width=800, height=600,
+    mapbox_style="open-street-map",
+    margin={"r": 0, "t": 0, "l": 0, "b": 0},
+    mapbox_center={"lat": 4.60971, "lon": -74.08175}
+)
 
 app.layout = html.Div([
-    html.Div([
-        dcc.Graph(
-            id='choropleth-map',
-            figure={
-                'data': [
-                    go.Choroplethmapbox(
-                        geojson=counties,
-                        locations=df.code,
-                        z=df.sampl,
-                        featureidkey='properties.DISPLAY_NAME',
-                        colorscale="Viridis",
-                        zmin=df.sampl.min(),
-                        zmax=df.sampl.max(),
-                        marker_opacity=0.5,
-                        marker_line_width=0
-                    )
-                ],
-                'layout': go.Layout(
-                    mapbox_style="open-street-map",
-                    mapbox_zoom=10,
-                    mapbox_center={"lat": 4.60971, "lon": -74.08175},
-                    margin={"r": 0, "t": 0, "l": 0, "b": 0},
-                    width=800,
-                    height=600
-                )
-            }
-        )
-    ])
+    
+    dcc.Graph(
+        id='basic-interactions',
+        figure=fig
+    ),
+
+     html.Button(id='submit-button-state', n_clicks=0, children='Submit'),
+     html.Div(id='output-state'),
+     html.Div([
+            dcc.Markdown( ),
+            html.Pre(id='click-data'),
+        ], className='three columns'),
 ])
+
+@app.callback(Output('output-state', 'children'),
+              Input('submit-button-state', 'n_clicks'))
+def update_output(n_clicks):
+    print("Entra")
+    return f'The Button has been pressed {n_clicks} times'
+
+@app.callback(
+    Output('click-data', 'children'),
+    Input('basic-interactions', 'clickData'))
+def display_click_data(clickData):
+    return json.dumps(clickData, indent=2)
 
 if __name__ == '__main__':
     app.run_server(debug=True)
-
 
 '''
 app.layout = html.Div([
