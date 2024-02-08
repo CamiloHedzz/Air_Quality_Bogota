6b1e7ebc793d205bcc64845b1ebd7f60061ede38
camilohedzz <camilohedzz@gmail.com>
2024-02-08 18:31:40 -0500

    adding regression graph

--- dash2/dash_app.py
+++ dash2/dash_app.py
@@ -6,6 +6,8 @@ from urllib.request import urlopen
 from django_plotly_dash import DjangoDash
 from dash import html, dcc, Output, Input, Patch
 
+import plotly.express as px
+
 app = DjangoDash('SimpleExample')
 
 with urlopen('https://gist.githubusercontent.com/john-guerra/ee93225ca2c671b3550d62614f4978f3/raw/b1d556c39f3d7b6e495bf26b7fda815765ac110a/bogota_cadastral.json') as response:
@@ -14,42 +16,39 @@ with urlopen('https://gist.githubusercontent.com/john-guerra/ee93225ca2c671b3550
 df = pd.read_csv("dash2/datasets/finalData.csv",
                    dtype={"code": str})
 
+df2 = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/gapminder_unfiltered.csv')
 
 
 selected_areas = []
 
-
-fig = go.Figure()
-
 app.layout = html.Div([
-    dcc.Graph(
-        id='basic-interactions',
-    )
+    html.Div([
+        dcc.Graph(id='bogota-map',)
+    ], style={ 'display': 'inline-block'}),
+    
+    html.Div([
+        dcc.Dropdown(df2.country.unique(), 'Canada', id='dropdown-selection'),
+        dcc.Graph(id='regression'),
+    ], style={'display': 'inline-block', 'width': '49%'}),
 ])
 
-
 @app.callback(
-    Output('basic-interactions', 'figure'),
-    [Input('basic-interactions', 'clickData')]
+    Output('bogota-map', 'figure'),
+    [Input('bogota-map', 'clickData')]
 )
 def update_map_on_click(clickData):
+    print("entra")
     global selected_areas
-    
+    feature_areas = {'op': [], 'wid': [], 'col': []}
     if clickData is not None:
         selected_area = clickData['points'][0]['location']
-
         if selected_area in selected_areas:
             selected_areas.remove(selected_area)
         else:
             selected_areas.append(selected_area)
     elif len(selected_areas)==0:
-        ale = df.sample(n=1)
-        selected_areas.append(ale['code'].values[0])
+        selected_areas.append(df.sample(n=1)['code'].values[0])
         
-        
-    print(selected_areas)   
-    feature_areas = {'op': [], 'wid': [], 'col': []}
-    
     for display_name in df.code.items():
         if display_name[1] in selected_areas:
             feature_areas['op'].append(1)
@@ -72,19 +71,65 @@ def update_map_on_click(clickData):
         marker_line_width=feature_areas['wid'],
         marker_line_color=feature_areas['col'],
         colorbar_title = "Particulas"
-
     ))
-    
+ 
     updated_fig.update_layout(
-        
         autosize=True,
-        width=800, height=600,
+        mapbox_zoom=11,
+        width=700, height=500,
         mapbox_style="open-street-map",
         margin={"r": 0, "t": 0, "l": 0, "b": 0},
         mapbox_center={"lat": 4.60971, "lon": -74.08175},
     )
-
+    
     return updated_fig
+
+@app.callback(
+    Output('regression', 'figure'),
+    Input('dropdown-selection', 'value'))
+def update_regression(value):
+    dff = df2[df2.country==value]
+    return px.line(dff, x='year', y='pop')
 
 if __name__ == '__main__':
     app.run_server(debug=True)
+
+
+'''
+
+#def create_regression(dff):
+    
+ #   pass
+
+
+def create_time_series(dff, axis_type, title):
+
+    fig = px.scatter(dff, x='Year', y='Value')
+
+    fig.update_traces(mode='lines+markers')
+
+    fig.update_xaxes(showgrid=False)
+
+    fig.update_yaxes(type='linear' if axis_type == 'Linear' else 'log')
+
+    fig.add_annotation(x=0, y=0.85, xanchor='left', yanchor='bottom',
+                       xref='paper', yref='paper', showarrow=False, align='left',
+                       text=title)
+
+    fig.update_layout(height=225, margin={'l': 20, 'b': 30, 'r': 10, 't': 10})
+
+    return fig
+
+
+@callback(
+    Output('x-time-series', 'figure'),
+    Input('crossfilter-indicator-scatter', 'hoverData'),
+    Input('crossfilter-xaxis-column', 'value'),
+    Input('crossfilter-xaxis-type', 'value'))
+def update_x_timeseries(hoverData, xaxis_column_name, axis_type):
+    country_name = hoverData['points'][0]['customdata']
+    dff = df[df['Country Name'] == country_name]
+    dff = dff[dff['Indicator Name'] == xaxis_column_name]
+    title = '<b>{}</b><br>{}'.format(country_name, xaxis_column_name)
+    return create_time_series(dff, axis_type, title)
+'''
