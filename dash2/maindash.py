909b55c7fa17e06966f31b63fcac6f2c97f5d059
camilohedzz <camilohedzz@gmail.com>
2024-05-03 21:40:34 -0500

    prediction html

--- dash2/maindash.py
+++ dash2/maindash.py
@@ -7,3 +7,5 @@ app = DjangoDash('SimpleExamplee', external_stylesheets=[dbc.themes.BOOTSTRAP])
 app.css.append_css({ "external_url" : "/static/css/dash_app.css" })
 
 app.css.append_css({ "external_url" : "/static/css/about_app.css" })
+
+app.css.append_css({ "external_url" : "/static/css/prediction_app.css" })
--- dash2/static/pages/predictions.py
+++ dash2/static/pages/predictions.py
@@ -1,3 +1,19 @@
 from dash import html, dcc
+import dash_bootstrap_components as dbc
 
-predictions = html.H1("Predicciones")
+predictions = html.Div( children=[
+            
+        dbc.Row([
+            dbc.Col(class_name="descripition_sarima"), 
+            
+            dbc.Col(class_name="graph_prediction")
+        ]),
+        
+        dbc.Row([
+            dbc.Col(class_name="graph_autocorrelation"),
+            
+            dbc.Col(class_name="graph_partial_autocorrelation")
+        ])                   
+                           
+                           
+], className="predict_container")
